df681518f92f688eb907aba7c385be1445d68bfd
arthurTemporim <arthurrtl@gmail.com>
2016-04-09 22:26:23 -0300

    Correct on global variables and 'if' and 'else'

--- pesquini/app/controllers/statistics_controller.rb
+++ pesquini/app/controllers/statistics_controller.rb
@@ -2,17 +2,18 @@ class StatisticsController < ApplicationController
 
 
 ######################################################
-# Declarações de variáveis Globais
+# Statements of global variables
 
-@@states_list = State.all_states
+@@STATES_LIST = State.all_states
 
 @@sanjana = Sanction.all_years
 
-@@sanction_type_list = SanctionType.all_sanction_types
+@@SANCTION_TYPE_LIST = SanctionType.all_sanction_types
 
 
 ######################################################
-# Métodos da controller
+
+# Methods from controller
 
   def  index
   end
@@ -46,15 +47,18 @@ class StatisticsController < ApplicationController
   end
 
   def sanction_by_state_graph
-    gon.states = @@states_list
+    gon.states = @@STATES_LIST
     gon.dados = total_by_state
     titulo = "Gráfico de Sanções por Estado"
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(:text => titulo)
-      if(params[:year_].to_i != 0)
+      if(params[:year_].to_i != 0){
          f.title(:text => params[:year_].to_i )
-       end
-      f.xAxis(:categories => @@states_list)
+      }
+      else {
+        #nothig to do
+      }
+      f.xAxis(:categories => @@STATES_LIST)
       f.series(:name => "Número de Sanções", :yAxis => 0, :data => total_by_state)
       f.yAxis [
       {:title => {:text => "Sanções", :margin => 30} },
@@ -89,10 +93,13 @@ class StatisticsController < ApplicationController
         })
     end
 
-    if (!@states)
-      @states = @@states_list.clone
+    if (!@states){
+      @states = @@STATES_LIST.clone
       @states.unshift("Todos")
-    end
+    }
+    else {
+      #nothing to do
+    }
     respond_to do |format|
       format.html # show.html.erb
       format.js
@@ -102,22 +109,23 @@ class StatisticsController < ApplicationController
 
 
 ######################################################
-# Métoodos auxiliares
+# This may go to one helper class
 
   def total_by_state()
     results = []
     @years = @@sanjana
-    @@states_list.each do |s|
+    @@STATES_LIST.each do |s|
       state = State.find_by_abbreviation("#{s}")
       sanctions_by_state = Sanction.where(state_id: state[:id])
       selected_year = []
-      if(params[:year_].to_i != 0)
+      if(params[:year_].to_i != 0){
         sanctions_by_state.each do |s|
-          if(s.initial_date.year ==  params[:year_].to_i)
+          if(s.initial_date.year ==  params[:year_].to_i){
             selected_year << s
-          end
-      end
+          }
+      }
         results << (selected_year.count)
+        #need to be revised
       else
         results << (sanctions_by_state.count)
       end
@@ -133,12 +141,12 @@ class StatisticsController < ApplicationController
 
     state = State.find_by_abbreviation(params[:state_])
 
-    @@sanction_type_list.each do |s|
+    @@SANCTION_TYPE_LIST.each do |s|
       sanction = SanctionType.find_by_description(s[0])
       sanctions_by_type = Sanction.where(sanction_type:  sanction)
-      if (params[:state_] && params[:state_] != "Todos")
+      if (params[:state_] && params[:state_] != "Todos"){
         sanctions_by_type = sanctions_by_type.where(state_id: state[:id])
-      end
+      }
       cont = cont + (sanctions_by_type.count)
       results2 << s[1]
       results2 << (sanctions_by_type.count)
@@ -146,11 +154,12 @@ class StatisticsController < ApplicationController
       results2 = []
     end
     results2 << "Não Informado"
-      if (params[:state_] && params[:state_] != "Todos")
+      if (params[:state_] && params[:state_] != "Todos"){
         total =Sanction.where(state_id: state[:id] ).count
-      else
+      }
+      else{
         total = Sanction.count
-      end
+      }
     results2 << (total - cont)
     results << results2
     results = results.sort_by { |i| i[0] }
