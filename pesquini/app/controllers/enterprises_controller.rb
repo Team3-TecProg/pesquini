994ed5561931a3c2838dd602f3668ce7bcb400cf
Lucas Amoêdo <lucas_advc@outlook.com>
2016-05-03 23:10:35 -0300

    Improve enterprises_controller readability

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -1,5 +1,7 @@
 class EnterprisesController < ApplicationController
+  
   def index
+
     if params[:q].nil?
       @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
       @enterprises = Enterprise.paginate(:page => params[:page], :per_page => 10)
@@ -11,22 +13,32 @@ class EnterprisesController < ApplicationController
   end
 
   def show
-    @per_page = 10
-    @page_num = params[:page].to_i > 0 ? params[:page].to_i  - 1 : 0
+
+    @results_per_page = 10
+
+    if (params[:page].to_i > 0)
+      @page_number = params[:page].to_i - 1
+    else
+      @page_number = 0
+    end
+
     @enterprise = Enterprise.find(params[:id])
     @collection = Sanction.where(enterprise_id: @enterprise.id)
-    @payments = Payment.where(enterprise_id: @enterprise.id).paginate(:page => params[:page], :per_page => @per_page )
-    @sanctions = @collection.paginate(:page => params[:page], :per_page => @per_page)
+    @payments = Payment.where(enterprise_id: @enterprise.id).paginate(:page => params[:page], :per_page => @results_per_page )
+    @sanctions = @collection.paginate(:page => params[:page], :per_page => @results_per_page)
     @payment_position = enterprise_payment_position(@enterprise)
     @position = Enterprise.enterprise_position(@enterprise)
   end
 
   def enterprise_payment_position(enterprise)
-    p = Enterprise.featured_payments  
-      p.each_with_index do |a, index|
-        if a.payments_sum == enterprise.payments_sum
-          return index + 1 
-        end
+
+    payment_list = Enterprise.featured_payments  
+    payment_list.each_with_index do |organization, index|
+      if organization.payments_sum == enterprise.payments_sum
+        return index + 1
+      else
+        # Nothing to do. 
       end
     end
   end
+end
