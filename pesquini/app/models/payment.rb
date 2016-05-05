7d34ea294c6be3b4684a1cda11b173f1282d0343
Lucas Amoêdo <lucas_advc@outlook.com>
2016-05-05 12:56:18 -0300

    Add header to payment.rb

--- pesquini/app/models/payment.rb
+++ pesquini/app/models/payment.rb
@@ -1,11 +1,18 @@
+# #####################################################################
+# Class name: Payment
+# File name: payment.rb
+# Description: Represents a payment done by an enterprise.
+######################################################################
+
 class Payment < ActiveRecord::Base
 
   belongs_to :enterprise
-
   validates_uniqueness_of :process_number
 
+
   def refresh!
-    p = Payment.find_by_process_number(self.process_number)
+  	# Reloads the Payment object.
+    Payment.find_by_process_number(self.process_number)
   end
 
 
