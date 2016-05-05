c25374570dec3421c6e3870b3831b22130e7fdd6
Lucas Amoêdo <lucas_advc@outlook.com>
2016-05-05 13:42:02 -0300

    Add header and comments to sessions_controller.rb

--- pesquini/app/controllers/sessions_controller.rb
+++ pesquini/app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
+######################################################################
+# Class name: SessionsController
+# File name: sessions_controller.rb
+# Description: class that implements default REST actions to User sessions.
+######################################################################
+
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
+    # Searches for a user that matches the provided login information.
     login = params[:session][:login].downcase
     password = params[:session][:password]
     user = User.find_by(login: login)
-
-    if user && user.authenticate(password)
+    # Verifies if the provided password is correct.
+    if (user && user.authenticate(password))
       sign_in user
       redirect_to root_path
     else
@@ -19,6 +25,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    # Finishes the user session.
     if signed_in?
       sign_out 
       redirect_to root_path
