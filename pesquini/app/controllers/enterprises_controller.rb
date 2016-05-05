e34758f690a7b05b3e233fbabb004e98920abb46
Lucas Amoêdo <lucas_advc@outlook.com>
2016-05-05 13:40:20 -0300

    Add comments to enterprises_controller and sanction_type

--- pesquini/app/controllers/enterprises_controller.rb
+++ pesquini/app/controllers/enterprises_controller.rb
@@ -1,18 +1,17 @@
 ######################################################################
 # Class name: EnterprisesController
 # File name: enterprises_controller.rb
-# Description: Controller for the Enterprises model, which holds --
-# -- methods to link it to the Enterprise views.
+# Description: Controller for the Enterprises model, which holds methods to 
+# link it to the Enterprise views.
 ######################################################################
 
 class EnterprisesController < ApplicationController
   
   def index
 
-    # Lists all the enterprises in the database, if the user did not provide --
-    # -- a query to search, or lists the enterprise that matches the --
-    # -- provided query otherwise.
-
+    # Lists all the enterprises in the database, if the user did not provide 
+    # a query to search, or lists the enterprise that matches the provided query 
+    # otherwise.
     if params[:q].nil?
       @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
       @enterprises = Enterprise.paginate(:page => params[:page], 
@@ -26,19 +25,14 @@ class EnterprisesController < ApplicationController
   end
 
   def show
-
     @results_per_page = 10
-
     # Matches a given Enterprise to its position in the page index. 
-
     if (params[:page].to_i > 0)
       @page_number = params[:page].to_i - 1
     else
       @page_number = 0
     end
-
     # Instance variables to be used in the Enterprise Show view.
-
     @enterprise = Enterprise.find(params[:id])
     @collection = Sanction.where(enterprise_id: @enterprise.id)
     @payments = Payment.where(enterprise_id: @enterprise.id).paginate(:page => 
@@ -50,20 +44,14 @@ class EnterprisesController < ApplicationController
   end
 
   def enterprise_payment_position(enterprise) 
-
     enterprises_ordered_by_payments = Enterprise.featured_payments  
-
     enterprises_ordered_by_payments.each_with_index do |organization, index|
-      
       # Returns the position of the Enterprise, based on its payment sum
-      
       if organization.payments_sum == enterprise.payments_sum
         return index + 1
       else
         # Nothing to do. 
       end
-
     end
-
   end
 end
--- pesquini/app/models/sanction_type.rb
+++ pesquini/app/models/sanction_type.rb
@@ -1,7 +1,7 @@
 ######################################################################
 # Class name: SanctionType
 # File name: sanction_type.rb
-# Description: Represents all the classifications for sanctions
+# Description: Represents all the classifications for sanctions.
 ######################################################################
 
 class SanctionType < ActiveRecord::Base
@@ -10,23 +10,33 @@ class SanctionType < ActiveRecord::Base
     validates_uniqueness_of :description
 
     def refresh!
+        # Reloads the Sanction object.
         actual_sanction = SanctionType.find_by_description(self.description)
     end
 
     def self.all_sanction_types
+        # The array containing all the possible sanction types is used for 
+        # statistics in StatisticsController.
         stantion_types = [
-        [ "INIDONEIDADE - LEGISLAçãO ESTADUAL", "Inidoneidade - Legislação Estadual"],
+        [ "INIDONEIDADE - LEGISLAçãO ESTADUAL", 
+            "Inidoneidade - Legislação Estadual"],
         [ "IMPEDIMENTO - LEI DO PREGãO", "Impedimento - Lei do Pregão"],
         [ "PROIBIçãO - LEI ELEITORAL", "Proibição - Lei Eleitoral"],
-        [ "INIDONEIDADE - LEI DE LICITAçõES","Inidoneidade - Lei de Licitações"],
-        [ "SUSPENSãO - LEI DE LICITAçõES","Suspensão - Lei de Impedimento Licitações"],
+        [ "INIDONEIDADE - LEI DE LICITAçõES",
+            "Inidoneidade - Lei de Licitações"],
+        [ "SUSPENSãO - LEI DE LICITAçõES",
+            "Suspensão - Lei de Impedimento Licitações"],
         [ "SUSPENSãO - LEGISLAçãO ESTADUAL", "Suspensão - Legislação estadual"],
         [ "PROIBIçãO - LEI DE IMPROBIDADE", "Proibição - Lei de improbidade"],
-        [ "DECISãO JUDICIAL LIMINAR/CAUTELAR QUE IMPEçA CONTRATAçãO","Decisão Judicial liminar"] ,
-        [ "INIDONEIDADE - LEI DA ANTT E ANTAQ ","Inidoneidade - Lei da ANTT e ANTAQ"] ,
+        [ "DECISãO JUDICIAL LIMINAR/CAUTELAR QUE IMPEçA CONTRATAçãO",
+            "Decisão Judicial liminar"] ,
+        [ "INIDONEIDADE - LEI DA ANTT E ANTAQ ",
+            "Inidoneidade - Lei da ANTT e ANTAQ"] ,
         [ "INIDONEIDADE - LEI ORGâNICA TCU", "Inidoneidade - Lei Orgânica TCU"],
-        [ "IMPEDIMENTO - LEGISLAçãO ESTADUAL", "Impedimento - Legislação Estadual"],
-        [ "SUSPENSãO E IMPEDIMENTO - LEI DE ACESSO à INFORMAçãO","Suspensão e Impedimento - Lei de Acesso à Informação"],
+        [ "IMPEDIMENTO - LEGISLAçãO ESTADUAL", 
+            "Impedimento - Legislação Estadual"],
+        [ "SUSPENSãO E IMPEDIMENTO - LEI DE ACESSO à INFORMAçãO",
+            "Suspensão e Impedimento - Lei de Acesso à Informação"],
         [ "PROIBIçãO - LEI ANTITRUSTE", "Proibição - Lei Antitruste"],
         [ "IMPEDIMENTO - LEI DO RDC", "Impedimento - Lei do RDC"],
         [ "PROIBIçãO - LEI AMBIENTAL", "Proibição - Lei Ambiental" ],
