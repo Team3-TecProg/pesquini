9e69fd7c4626a0b90fa3874b7b1bcdc1398e4a7f
Sabryna <sabryna.sousa1323@gmail.com>
2016-05-05 22:29:25 -0300

    Add assertives in sessions_controller

--- pesquini/app/controllers/sessions_controller.rb
+++ pesquini/app/controllers/sessions_controller.rb
@@ -5,30 +5,34 @@
 ######################################################################
 
 class SessionsController < ApplicationController
-
-  def new
-  end
-
-  def create
-    # Searches for a user that matches the provided login information.
-    login = params[:session][:login].downcase
-    password = params[:session][:password]
-    user = User.find_by(login: login)
-    # Verifies if the provided password is correct.
-    if (user && user.authenticate(password))
-      sign_in user
-      redirect_to root_path
-    else
-      flash[:error] = "Login ou senha invalidos!"
-      render :new
+    def new
     end
-  end
 
-  def destroy
-    # Finishes the user session.
-    if signed_in?
-      sign_out 
-      redirect_to root_path
+    def create
+        # Searches for a user that matches the provided login information.
+        login = params[:session][:login].downcase
+        assert_object_is_not_null ( login )
+        password = params[:session][:password]
+        assert_object_is_not_null ( password )
+        user = User.find_by(login: login)
+        assert_object_is_not_null ( user )
+        # Verifies if the provided password is correct.
+        if ( user && user.authenticate( password ) )
+            sign_in user
+            redirect_to root_path
+        else
+            flash[:error] = "Login ou senha invalidos!"
+            render :new
+        end
     end
-  end
+
+    def destroy
+        # Finishes the user session.
+        if signed_in?
+            sign_out
+            redirect_to root_path
+        else
+            # Nothing to do.
+        end
+    end
 end
