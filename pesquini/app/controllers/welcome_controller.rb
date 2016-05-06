5b70429a28861867acb389f1c273f332fa50162a
Sabryna <sabryna.sousa1323@gmail.com>
2016-05-05 21:24:59 -0300

    Add comments in welcome_controller

--- pesquini/app/controllers/welcome_controller.rb
+++ pesquini/app/controllers/welcome_controller.rb
@@ -1,7 +1,15 @@
+######################################################################
+# Class name: WelcomeController
+# File name: welcome_controller.rb
+# Description: Represents the initial page of the application
+######################################################################
+
 class WelcomeController < ApplicationController
-  def index
-    params[:q][:cnpj_eq] = params[:q][:corporate_name_cont] unless params[:q].nil?
-    @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
-    @enterprises = @search.result
-  end
+    # Searches for an Enterprise from the database
+    # according to the information provided by the user.
+    def index
+        params[:q][:cnpj_eq] = params[:q][:corporate_name_cont] unless params[:q].nil?
+        @search = Enterprise.search(params[:q].try(:merge, m: 'or'))
+        @enterprises = @search.result
+    end
 end
