6bbf8574d8abe3adc54ac6baddc1eddca6495e22
Matheus Miranda <matheusmirandalacerda@gmail.com>
2016-05-23 11:27:30 -0300

    Improve spec tests for model state

--- pesquini/spec/models/state_spec.rb
+++ pesquini/spec/models/state_spec.rb
@@ -2,55 +2,64 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe State do
-
-  before do
-    @state = State.new
-    @state.abbreviation = "DF1"
-    @state.save
-  end
-
-  subject {@state}
-    it { should respond_to(:abbreviation) }
-    it { should respond_to(:id) }
-    it { should respond_to(:code) }
-    it { should respond_to(:name) }
-  it { should be_valid }
-
-  describe "uniqueness validation of abbreviation" do
-    
-    describe "unique abbreviation" do
-      it "should be_valid" do
-        uniqueness_state = State.new
-        uniqueness_state.abbreviation = "SP1"
-        uniqueness_state.save 
-        expect(uniqueness_state).to be_valid
-      end
+    before do
+        @state = State.new
+        @state.abbreviation = "DF1"
+        @state.save
     end
 
-   describe "duplicated abbreviation" do
+    subject {@state}
+        it { should respond_to(:abbreviation) }
+        it { should respond_to(:id) }
+        it { should respond_to(:code) }
+        it { should respond_to(:name) }
+    it { should be_valid }
+
+    describe "uniqueness validation of abbreviation" do
+
+    describe "unique abbreviation" do
+        it "should be_valid" do
+            uniqueness_state = State.new
+            uniqueness_state.abbreviation = "SP1"
+            uniqueness_state.save
+            expect(uniqueness_state).to be_valid
+        end
+    end
+
+    describe "duplicated abbreviation" do
         it "should not be_valid" do
-        duplicated_state = State.new
-        duplicated_state.abbreviation = "DF1" 
-        expect(duplicated_state).not_to be_valid
-      end
+            duplicated_state = State.new
+            duplicated_state.abbreviation = "DF1"
+            expect(duplicated_state).not_to be_valid
+        end
     end
 
 
     describe "#refresh!" do
-      before do
-        @s = State.new
-        @s.abbreviation = "Es_Teste"
-        @s.save
-      end
+        before do
+            @new_state = State.new
+            @new_state.abbreviation = "Es_Teste"
+            @new_state.save
+        end
 
-      it "should return state" do
-        expect(@s.refresh!).to eq(@s);
-      end
+        it "should return state" do
+            expect(@new_state.refresh!).to eq(@new_state);
+        end
 
-      it "should not return other state" do
-        expect(@s.refresh!).not_to eq(@state);
-      end  
+        it "should not return other state" do
+            expect(@new_state.refresh!).not_to eq(@state);
+        end
     end
-    
+
+    describe "#get_all_states" do
+        expected_return = [ "BA", "DF", "RJ", "PA", "MG", "SP",
+        "AM", "RS", "SC","ES", "PR", "PB", "RN", "CE", "AL", "RR", "SE",
+        "RO","PI" , "AC","TO", "GO", "PE", "AP", "MS", "MT", "MA",
+        "Não Informado" ]
+
+        it "should return an array with all states" do
+            expect(State.get_all_states).to eq(expected_return)
+        end
+    end
   end
 end
