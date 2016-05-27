e6618c439941a07e66d16f0e635b2d9ddc4285bd
Sabryna <sabryna.sousa1323@gmail.com>
2016-05-27 17:37:01 -0300

    Apply style sheet in teste payment

--- pesquini/spec/models/payment_spec.rb
+++ pesquini/spec/models/payment_spec.rb
@@ -14,8 +14,8 @@ describe Payment do
         @payment.save
     end
 
-    subject {@payment}
-    it { should respond_to(:process_number) }
+    subject { @payment }
+    it { should respond_to( :process_number ) }
     it { should be_valid }
 
     describe "uniqueness validation of process_number" do
@@ -32,23 +32,23 @@ describe Payment do
         it "should not be_valid" do
             duplicated_payment = Payment.new
             duplicated_payment.process_number = "1"
-            expect(duplicated_payment).not_to be_valid
+            expect( duplicated_payment ).not_to be_valid
         end
     end
 
     describe "#update" do
         before do
-            @p = Payment.new
-            @p.process_number = "437924"
-            @p.save
+            @payment_update = Payment.new
+            @payment_update.process_number = "437924"
+            @payment_update.save
         end
 
         it "should return Payment" do
-            expect(@p.update_payment).to eq(@p);
+            expect( @payment_update.update_payment ).to eq( @payment_update );
         end
 
         it "should not return other Payment" do
-            expect(@p.update_payment).not_to eq(@payment);
+            expect( @payment_update.update_payment ).not_to eq( @payment );
         end
     end
   end
