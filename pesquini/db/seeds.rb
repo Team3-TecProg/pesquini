63bb5d0b843ab893b7f0903ef2efe6d2189ce7a0
ArthurTemporim <arthurrtl@gmail.com>
2016-05-29 12:13:58 -0300

    Seeds made to sanctionType

--- pesquini/db/seeds.rb
+++ pesquini/db/seeds.rb
@@ -7,13 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #State
+#In Brazil have 26 states and DF, so 27 states.
 27.times do |i|
   State.create(code: "#{i}", name: "Estado ##{i}", abbreviation: "Ab. ##{i}")
 end
 
 #Enterprise
+#Create 5 generic enterprises.
 5.times do |i|
   Enterprise.create(cnpj: "CNPJ ##{i}", corporate_name: "corporate_name ##{i}")
 end
 
-
+#SanctionType
+#create 5 generic sanctions types.
+5.times do |i|
+	SanctionType.create(description: "Descrição ##{i}",
+						legal_foundation: "Fundamento legal ##{i}",
+						foundation_description: "Descrição do fundamento ##{i}")
+end
