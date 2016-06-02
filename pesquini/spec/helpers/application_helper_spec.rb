fe8796a0973cb1940ea2ddbf25cef9ceec6c7349
Sabryna <sabryna.sousa1323@gmail.com>
2016-06-02 15:06:10 -0300

    Apply style sheet and add header in helper tests

--- pesquini/spec/helpers/application_helper_spec.rb
+++ pesquini/spec/helpers/application_helper_spec.rb
@@ -1,21 +1,27 @@
+######################################################################
+# File name: application_helper_spec.rb.
+# Description: This file contains all units tests for the application
+# helper.
+######################################################################
+
 require 'rails_helper'
-  
+
 include ApplicationHelper
 
 RSpec.describe ApplicationHelper, :type => :helper do
   describe '#flash_class' do
     it "should return alert alert-info" do
-      expect(flash_class(:notice)).to eq('alert alert-info')
+      expect( flash_class( :notice ) ).to eq( 'alert alert-info' )
       end
 
        it "should return alert alert-success" do
-      expect(flash_class(:success)).to eq('alert alert-success')
+      expect( flash_class( :success ) ).to eq( 'alert alert-success' )
     end
      it "should return alert alert-error" do
-      expect(flash_class(:error)).to eq('alert alert-error')
+      expect( flash_class( :error ) ).to eq( 'alert alert-error' )
     end
      it "should return alert alert-error" do
-      expect(flash_class(:alert)).to eq('alert alert-error')
+      expect( flash_class( :alert ) ).to eq( 'alert alert-error' )
     end
   end
 
--- pesquini/spec/helpers/session_helper_spec.rb
+++ pesquini/spec/helpers/session_helper_spec.rb
@@ -1,32 +1,38 @@
+######################################################################
+# File name: session_helper_spec.rb.
+# Description: This file contains all units tests for the session
+# helper.
+######################################################################
+
 require 'rails_helper'
 
 include SessionsHelper
 
 RSpec.describe SessionsHelper, :type => :helper do
-  user = User.new(login: 'sanjaninha', password: 'sanjana123')
+  user = User.new( login: 'sanjaninha', password: 'sanjana123' )
   user.save
 
   describe '#current_user?' do
     context 'compared with @user' do
       it 'must return true' do
-        expect(current_user).to eq(@user)
+        expect( current_user ).to eq( @user )
       end
     end
   end
 
   describe '#sign_out' do
       it 'the user should logout with success' do
-        sign_in(user)
+        sign_in( user )
         sign_out
-        expect(current_user).to be(nil)
+        expect( current_user ).to be( nil )
     end
   end
 
   describe '#signed_in?' do
     it "expect signout to clear current_user" do
-      sign_in(user)
+      sign_in( user )
       sign_out
-      expect(signed_in?).to be(false)
+      expect( signed_in? ).to be( false )
     end
   end
 end
