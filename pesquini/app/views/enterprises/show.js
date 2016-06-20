a0734eb7a3d10630eef369ead19b7d3c1ef7b4bd
Matheus Miranda <matheusmirandalacerda@gmail.com>
2016-06-20 11:42:10 -0300

    Add internationalization and improve identation at view show

--- pesquini/app/views/enterprises/show.js
+++ pesquini/app/views/enterprises/show.js
@@ -1,11 +1,7 @@
+var sanctions_btn = document.getElementById("sanction-info-btn");
 
-  var sanctions_btn = document.getElementById("sanction-info-btn");
-
-if ($(sanctions_btn).hasClass("active"))
-{
-  $(".sanction-info").html("<%= escape_javascript(render("sanctions")) %>");
-}
-else
-{
-  $(".payment-info").html("<%= escape_javascript(render("payments")) %>");
+if ($(sanctions_btn).hasClass("active")){
+   $(".sanction-info").html("<%= escape_javascript(render("sanctions")) %>");
+}else{
+    $(".payment-info").html("<%= escape_javascript(render("payments")) %>");
 }
